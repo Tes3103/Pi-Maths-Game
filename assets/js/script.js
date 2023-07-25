de84b41062e4e2febbd0a1f37fd65e7e7cc80bb0
Tesfaye Wolde <tesfekadu21@gmail.com>
2023-07-25 19:18:09 +0000

    Add basic structure of the game function

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("you clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert (`you clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+
+function runGame() {
+
+}
+
+
+function checkAnswer() {
+
+}
+
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incremetScore() {
+    
+}
+
+function incremetWrongAnswers() {
+    
+}
+
+function displayGreaterthanQuestion() {
+    
+}
+
+function displayEqualQuestion() {
+    
+}
+
+function displayLessthanQuestion() {
+    
+}
